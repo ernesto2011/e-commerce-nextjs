827757acf743ccadd674ec87a1f2831107ffa444
ernesto2011 <skillmx107@gmail.com>
2025-01-26 22:58:09 -0600

    .

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import { TopMenu } from "@/components";
+import React from "react";
 
-export default function layout({children}:{
-    children: React.ReactNode
-}) {
+export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-        {
-        children}
+      <TopMenu />
+      {children}
     </>
-    
-)
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body
-        className={` antialiased`}
       >
         {children}
       </body>
