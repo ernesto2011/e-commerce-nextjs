db081bcc46b98b3413c7b54019e84a3a2381dff2
ernesto2011 <skillmx107@gmail.com>
2025-01-27 22:50:23 -0600

    mostrar productos, grid y card

---
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { Product } from "@/interfaces"
+import Image from "next/image"
+import Link from "next/link"
+import { useState } from "react"
+
+interface Props{
+    product: Product
+}
+export const ProductGridItem = ({product}:Props) => {
+    const [displayImage, setDisplayImage] = useState(product.images[0])
+    
+  return (
+    <div className="rounded-md overflow-hidden fade-in">
+        <Link href={`/product/${product.slug}`}>
+        <Image src={`/products/${displayImage}`}
+        onMouseEnter={() => setDisplayImage(product.images[1]?? product.images[0])}
+        onMouseLeave={() => setDisplayImage(product.images[0])}
+        alt={product.title} width={300} height={300} className="rounded-md"/>
+        </Link>
+        <div className="p-4 flex flex-col">
+            <Link 
+            className="hover:text-blue-600 transition-colors"
+            href={`/product/${product.slug}`}>{product.title}</Link>
+            <span className="font-bold">${product.price}</span>
+        </div>	
+    </div>
+  )
+}
