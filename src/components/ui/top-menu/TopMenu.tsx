f6e304377f4d2f1083381df8b8d307127f4053ef
ernesto2011 <skillmx107@gmail.com>
2025-02-06 22:17:36 -0600

    counter of products in cart

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,11 +1,17 @@
 'use client'
 import { titleFont } from "@/app/fonts/fonts"
-import { useUIStore } from "@/store";
+import { useCartStore, useUIStore } from "@/store";
 import Link from "next/link"
+import { useEffect, useState } from "react";
 import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
 
 export const TopMenu = () => {
   const openMenu = useUIStore(state => state.openSideMenu)
+  const totalItemsInCart = useCartStore(state =>state.getTotalItems())
+  const [loading, setLoading]= useState(false)
+  useEffect(()=>{
+    setLoading(true)
+  },[])
   return (
     <nav className="flex px-5 justify-between items-center w-full">
         <div>
@@ -28,7 +34,9 @@ export const TopMenu = () => {
             </Link>
             <Link href={'/cart'} className="mx-2">
             <div className="relative">
-              <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-4 h-4">1</span>
+              {loading && (totalItemsInCart > 0) && (
+              <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-4 h-4">{totalItemsInCart}</span>) 
+              }
               <IoCartOutline className="w-5 h-5"/>
             </div>
             </Link>
--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -4,12 +4,17 @@ import { persist } from "zustand/middleware";
 
 interface State {
     cart: CartProduct[];
+    getTotalItems: () => number;
     addProductToCart: (product: CartProduct) => void;
 }
 export const useCartStore = create<State>()(
     persist(
         (set, get) =>({
             cart: [],
+            getTotalItems: () => {
+                const { cart } = get();
+                return cart.reduce((total, item) => total + item.quantity, 0);
+            },
             addProductToCart: (product: CartProduct) => {
                 const { cart } = get();
                 const productInCart = cart.some(item => item.id === product.id && item.size === product.size);
